662dc6764e1af300072d4bd963a0f62f1fc58b93
kanekotetsuya <tetsuyan78@i.softbank.jp>
2017-08-02 21:37:03 +0900

    section04 to 4.1

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -7,4 +7,55 @@ class ViewController < ApplicationController
   def form_for
     @book = Book.new
   end
+
+  def field
+    @book = Book.new
+  end
+
+  def html5
+    @book = Book.new
+  end
+
+  def select
+    @book = Book.new(publish: '技術評論社')
+  end
+
+  def col_select
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def group_select
+    @review = Review.new
+    @authors = Author.all
+  end
+
+  def select_tag
+  end
+
+  def select_tag2
+  end
+
+  def col_select2
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def group_select2
+    @authors = Author.all
+  end
+
+  def dat_select
+    @book = Book.find(1)
+  end
+
+  def col_radio
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def fields
+    @user = User.find(1)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,17 @@ Rails.application.routes.draw do
   post 'keyword/search'
   get 'view/form_tag'
   get 'view/form_for'
+  get 'view/field'
+  get 'view/html5'
+  get 'view/select'
+  get 'view/col_select'
+  get 'view/group_select'
+  get 'view/select_tag'
+  get 'view/select_tag2'
+  get 'view/col_select2'
+  get 'view/group_select2'
+  get 'view/dat_select'
+  get 'view/col_radio'
+  get 'view/fields'
 
 end
